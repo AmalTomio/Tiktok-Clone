28f0a0df8ebf0f3dbe76f0a2b5d117895f1a0d29
@amal7784404 <amal@trackerhero.com>
2025-02-11 18:07:13 +0800

    Video test

---
+++ tiktok-frontend/layouts/UploadLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-[#F8F8F8]">
+    <TopNav />
+
+    <div class="flex justify-between mx-auto w-full px-2 max-w-[1140px]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
