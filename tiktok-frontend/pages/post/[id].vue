ca89ab53d63af3a8a6b61f7b3b5f6ed12c2c9e4e
@amal7784404 <amal@trackerhero.com>
2025-02-21 17:38:47 +0800

    update comment section

--- tiktok-frontend/pages/post/[id].vue
+++ tiktok-frontend/pages/post/[id].vue
@@ -41,7 +41,7 @@
       ></video>
 
       <div
-        v-if="true"
+        v-if="!isLoaded"
         class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-70 z-10"
       >
         <Icon
@@ -88,7 +88,152 @@
             </div>
           </div>
         </div>
+
+        <Icon
+          v-if="true"
+          @click="($event) => deletePost()"
+          class="cursor-pointer"
+          name="material-symbols:delete-outline-sharp"
+          size="25"
+        />
+      </div>
+
+      <div class="px-8 mt-4 text-sm">This is the post text</div>
+      <div class="px-8 mt-4 text-sm font-bold flex items-center space-x-2">
+        <Icon name="mdi:music" size="17" />
+        <span>Original sound - User name</span>
+      </div>
+
+      <div class="flex items-center px-8 mt-8">
+        <div class="text-center flex items-center">
+          <button
+            class="rounded-full bg-gray-200 p-2 flex items-center justify-center"
+          >
+            <Icon name="mdi:heart" size="25" />
+          </button>
+
+          <span class="text-xs pl-2 pr-4 text-gray-800 font-semibold">123</span>
+        </div>
+
+        <div class="text-center flex items-center">
+          <button
+            class="rounded-full bg-gray-200 p-2 flex items-center justify-center"
+          >
+            <Icon name="bx:bxs-message-rounded-dots" size="25" />
+          </button>
+
+          <span class="text-xs pl-2 pr-4 text-gray-800 font-semibold">123</span>
+        </div>
+      </div>
+
+      <div
+        id="Comments"
+        class="bg-[#F8F8F8] z-0 w-full h-[calc(100%-273px)] border-t-2 overflow-auto mt-4"
+      >
+        <div class="pt-2" />
+
+        <div v-if="false" class="text-center mt-6 text-xl text-gray-500">
+          No comments...
+        </div>
+
+        <div v-else class="flex items-center justify-between px-8 mt-4">
+          <div class="flex items-center relative w-full">
+            <NuxtLink to="/">
+              <img
+                class="absolute top-0 rounded-full lg:mx-0 mx-auto"
+                width="40"
+                src="https://picsum.photos/id/8/300/320"
+              />
+            </NuxtLink>
+
+            <div class="ml-14 pt-0.5 w-full">
+              <div
+                class="text-[18px] font-semibold flex items-center justify-between"
+              >
+                User name
+                <Icon
+                  v-if="true"
+                  @click="($event) => deleteComment()"
+                  class="cursor-pointer"
+                  name="material-symbols:delete-outline-sharp"
+                  size="25"
+                />
+              </div>
+
+              <div class="text-[15px] font-light">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus aliquid blanditiis fugiat. Nihil, labore quam sunt,
+                animi quis maiores tempora delectus sed dolore voluptatibus modi
+                tempore suscipit non voluptate deleniti!
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mb-28" />
+      </div>
+
+      <div
+        id="CreateComment"
+        v-if="true"
+        class="absolute flex items-center justify-between bottom-0 bg-white h-[85px] lg:max-w-[550px] w-full py-5 px-8 border-t-2"
+      >
+        <div
+          :class="
+            inputFocused
+              ? 'border-2 border-gray-400'
+              : 'border-2 border-[#F1F1F2]'
+          "
+          class="bg-[#F1F1F2] flex items-center rounded-lg w-full lg:max-w-[420px] transition-all duration-200"
+        >
+          <input
+            v-model="comment"
+            @focus="inputFocused = true"
+            @blur="inputFocused = false"
+            class="bg-[#F1F1F2] text-[14px] focus:outline-none w-full lg:max-w-[420px] p-2 rounded-lg"
+            type="text"
+            placeholder="Write a comment... "
+          />
+        </div>
+
+        <button
+          :disabled="!comment"
+          @click="($event) => addComment()"
+          :class="comment ? 'text-[#F02C56] cursor-pointer' : 'text-gray-400'"
+          class="font-semibold text-sm ml-5 pr-1"
+        >
+          Post
+        </button>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+const route = useRoute();
+const router = useRouter();
+
+let video = ref(null);
+let isLoaded = ref(false);
+let comment = ref(null);
+let inputFocused = ref(false);
+
+onMounted(() => {
+  isLoaded.value = true; // Set to true when the video is loaded
+  video.value.play(); // Play the video after it's loaded
+});
+
+onBeforeUnmount(() => {
+  video.value.pause();
+  video.value.currentTime = 0;
+  video.value.src = "";
+});
+
+watch(
+  () => isLoaded.value,
+  () => {
+    if (isLoaded.value) {
+      setTimeout(() => video.value.play(), 500);
+    }
+  }
+);
+</script>
